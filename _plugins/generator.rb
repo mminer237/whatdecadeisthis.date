d2f47dc2aabe7bfb86d9642274413b366eeab649
Matthew Miner <mminer237@gmail.com>
2019-12-30 23:01:08 -0600

    Added year generator

---
+++ _plugins/generator.rb
@@ -0,0 +1,48 @@
+module Jekyll
+	def self.ordinal(n)
+		n = n.to_i.abs
+
+		if (11..13).include?(n % 100)
+			"th"
+		else
+			case n % 10
+				when 1 then "st"
+				when 2 then "nd"
+				when 3 then "rd"
+				else        "th"
+			end
+		end
+	end
+
+	class YearPage < Page
+		def initialize(site, base, dir, name)
+			@site = site
+			@base = base
+			@dir = dir
+			@name = name
+			self.process(@name)
+			self.data ||= {}
+			self.data['layout'] = 'year'
+			year = Integer(name.rpartition('.').first)
+			self.data['title'] = year
+			decade = (year / 10.0).ceil()
+			self.data['decade'] = decade.to_s + Jekyll.ordinal(decade)
+			self.data['digitDecade'] = (year / 10 + 1).to_s + '0s'
+		end
+	end
+
+	class YearPageGenerator < Generator
+		def generate(site)
+			years = (1..Integer(site.config['lastYear']))
+			years.each do |year|
+				name = "#{year}.md"
+				puts name
+				page = Jekyll::YearPage.new(site, site.source, @dir, name)
+				# page.data['title'] = year
+				# page.data['layout'] = 'year'
+				# page.content = "This is #{year}"
+				site.pages << page
+			end
+		end
+	end
+end
