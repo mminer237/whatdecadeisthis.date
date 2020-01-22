cb1093643581b960859a059ec228187ea133ccb2
Matthew Miner <mminer237@gmail.com>
2020-01-21 23:33:08 -0500

    Generate decades

--- _plugins/generator.rb
+++ _plugins/generator.rb
@@ -53,4 +53,55 @@ module Jekyll
 			end
 		end
 	end
+
+	class OrdinalDecadePage < Page
+		def initialize(site, base, dir, name, decade)
+			@site = site
+			@base = base
+			@dir = dir
+			@name = name
+			self.process(@name)
+			self.data ||= {}
+			self.data['layout'] = 'ordinal-decade'
+			self.data['ordinal'] = Jekyll.ordinal(decade)
+			self.data['title'] = "#{decade}#{self.data['ordinal']} Decade"
+			self.data['decade'] = decade
+		end
+	end
+
+	class CardinalDecadePage < Page
+		def initialize(site, base, dir, name)
+			@site = site
+			@base = base
+			@dir = dir
+			@name = name
+			self.process(@name)
+			self.data ||= {}
+			self.data['layout'] = 'cardinal-decade'
+			decade_name = name.rpartition('.').first
+			self.data['title'] = decade_name
+			self.data['first_year'] = Integer(decade_name.rpartition('s').first)
+		end
+	end
+
+	class DecadePageGenerator < Generator
+		def generate(site)
+			decades = (1..(Integer((site.config['lastYear']) / 10.0).ceil() + 1))
+			decades.each do |decade|
+				ordinal_name = "#{decade}#{Jekyll.ordinal(decade)}-decade.md"
+				puts ordinal_name
+				page = Jekyll::OrdinalDecadePage.new(site, site.source, @dir, ordinal_name, decade)
+				site.pages << page
+
+				if decade == 1
+					cardinal_name = "0s.md"
+				else
+					cardinal_name = "#{decade - 1}0s.md"
+				end
+				puts cardinal_name
+				page = Jekyll::CardinalDecadePage.new(site, site.source, @dir, cardinal_name)
+				site.pages << page
+			end
+		end
+	end
 end
